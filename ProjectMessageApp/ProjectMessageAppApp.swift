524bafcb6a4d52668f4e25e31b0678d486f26ba9
Hanad Eid <hanad.eid@outlook.com>
2024-12-04 01:25:46 +0000

    Initial Commit

---
+++ ProjectMessageApp/ProjectMessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectMessageAppApp.swift
+//  ProjectMessageApp
+//
+//  Created by Ibrahim Eid on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectMessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectMessageAppTests/ProjectMessageAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectMessageAppTests.swift
+//  ProjectMessageAppTests
+//
+//  Created by Ibrahim Eid on 04/12/2024.
+//
+
+import Testing
+
+struct ProjectMessageAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
